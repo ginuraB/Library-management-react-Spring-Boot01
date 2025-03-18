d38d341323a416b0925f5aa683c134274647713a
ginuraB <uniginura@gmail.com>
2025-03-18 11:07:46 +0530

    SearchBooksPage  done
    SearchBooksPage  done now gonna add pagination to the below

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,11 +3,12 @@ import "./App.css";
 import { Navbar } from "./layouts/Navbar And Footer/Navbar";
 import { Footer } from "./layouts/Navbar And Footer/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 export const App = () => {
   return (
     <div>
-      <Navbar />;
-      <HomePage />
+      <Navbar />;{/* <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </div>
   );
