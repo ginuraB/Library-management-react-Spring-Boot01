489cbca0b8767628226838918d130e67fccf01ca
ginuraB <uniginura@gmail.com>
2025-03-18 12:28:24 +0530

    paginating as thought
    scroll to the top functionality done
    static data bug fixed

--- 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -3,17 +3,23 @@ import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from ".././Utils/SpinnerLoading";
 import { SearchBook } from "./components/SearchBook";
+import { Pagination } from "../Utils/Pagination";
 
 export const SearchBooksPage = () => {
   const [books, setBooks] = useState<BookModel[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [httpError, setHttpError] = useState<string | null>(null);
-
+  const [currentpage, setCurrentPage] = useState(1);
+  const [booksPerPage] = useState(5);
+  const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   useEffect(() => {
     const fetchBooks = async () => {
       try {
         const baseUrl: string = "http://localhost:8080/api/books";
-        const url: string = `${baseUrl}?page=0&size=5&projection=bookProjection`;
+        const url: string = `${baseUrl}?page=${
+          currentpage - 1
+        }&size=${booksPerPage}&projection=bookProjection`;
         const response = await fetch(url);
 
         if (!response.ok) {
@@ -23,8 +29,8 @@ export const SearchBooksPage = () => {
         const responseJson = await response.json();
         const responseData = responseJson._embedded.books;
 
-        // Debugging: Log the API response to check if images exist
-        console.log("Fetched Books:", responseData);
+        setTotalAmountOfBooks(responseJson.page.totalElements);
+        setTotalPages(responseJson.page.totalPages);
 
         const loadedBooks: BookModel[] = responseData.map((book: any) => ({
           id: book.id,
@@ -46,7 +52,8 @@ export const SearchBooksPage = () => {
     };
 
     fetchBooks();
-  }, []);
+    window.scrollTo(0, 0);
+  }, [currentpage]);
 
   if (isLoading) {
     return <SpinnerLoading />;
@@ -59,6 +66,15 @@ export const SearchBooksPage = () => {
       </div>
     );
   }
+
+  const indexOfLastBook: number = currentpage * booksPerPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+  let lastitem =
+    booksPerPage * currentpage <= totalAmountOfBooks
+      ? booksPerPage * currentpage
+      : totalAmountOfBooks;
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
     <div>
@@ -124,12 +140,25 @@ export const SearchBooksPage = () => {
             </div>
           </div>
           <div className="mt-3">
-            <h5>Number of Results:(22)</h5>
+            <h5>Number of Results:({totalAmountOfBooks})</h5>
           </div>
-          <p> 1 to 5 of 22 items:</p>
+          <p>
+            {" "}
+            {indexOfFirstBook + 1} to {lastitem} of {totalAmountOfBooks} items:
+          </p>
           {books.map((book) => (
             <SearchBook book={book} key={book.id} />
           ))}
+
+          {/* if thotal pages >1 then render below says tat */}
+
+          {totalPages > 1 && (
+            <Pagination
+              currentPage={currentpage}
+              totalPages={totalPages}
+              paginate={paginate}
+            />
+          )}
         </div>
       </div>
     </div>
