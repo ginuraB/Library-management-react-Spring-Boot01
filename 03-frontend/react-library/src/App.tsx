735c20af388b5f2ea2904f6c43f18ddcf55cb240
ginuraB <uniginura@gmail.com>
2025-03-20 20:53:49 +0530

    till 10 route secure route part   all things are done by the book
    have not add routes secure routs

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,10 +4,31 @@ import { Navbar } from "./layouts/Navbar And Footer/Navbar";
 import { Footer } from "./layouts/Navbar And Footer/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
-import { Navigate, redirect, Route, Routes } from "react-router-dom";
+import { Navigate, redirect, Route, Routes, useNavigate } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
+//import OktaAuth from "@okta/okta-auth-js";
+import { oktaConfig } from "./lib/oktaConfig";
+import {OktaAuth, toRelativeUrl} from "@okta/okta-auth-js";
+
+
+const oktaAuth = new OktaAuth(oktaConfig);  
+ 
+
+
 export const App = () => {
+
+  const customAuthHandler = () => {
+    const Navigate = useNavigate();
+    const handleLoginRedirect = () => {
+    Navigate("/login"); // Navigate to the "/login" route
+  }
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: string) => {
+  const navigate = useNavigate(); // Use the navigate function from React Router v6
+  navigate(toRelativeUrl(originalUri || "/", window.location.origin), { replace: true });
+};
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />;
