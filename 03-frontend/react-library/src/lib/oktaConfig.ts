284a355f4da973f1f2f9f9b6199084b999dfb1d4
ginuraB <uniginura@gmail.com>
2025-03-20 14:38:12 +0530

    authentication part
    section 18    0007   05.07 min

---
+++ 03-frontend/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: process.env.REACT_APP_OKTA_CLIENT_ID || '',
+    issuer: process.env.REACT_APP_OKTA_ISSUER || '',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true
+};
